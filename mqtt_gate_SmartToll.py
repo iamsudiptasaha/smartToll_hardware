f264164a3cfab343b34e3fcbac059b18f9f39693
Sudipta Saha <subho040995@gmail.com>
2018-04-30 17:28:15 +0530

    Fixed MQTT Bug

--- mqtt_gate_SmartToll.py
+++ mqtt_gate_SmartToll.py
@@ -95,7 +95,7 @@ client = mqtt.Client()
 client.on_connect = on_connect
 client.on_message = on_message
 
-#client.connect("test.mosquitto.org", 1883, 60)
+#client.connect("<broker_id>", <broker_port>, <timeout>)
 
 
 
